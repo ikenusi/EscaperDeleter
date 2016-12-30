24e968ff58d3e8e496f689365cd25c57d50ac4a4
ikenusi <ikenusi.ikenusi@gmail.com>
2016-12-30 20:19:59 +0900

    公開

---
+++ src/EscaperDeleter/EscaperDeleter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EscaperDeleter;
+
+
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\Listener;
+
+use pocketmine\plugin\PluginBase;
+
+class EscaperDeleter extends PluginBase implements Listener{
+
+	public function onLoad(){
+	}
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onDisable(){
+	}
+
+	public function onQuit(PlayerQuitEvent $event){
+		$p = $event->getPlayer();
+		$s = $this->getServer();
+		foreach($s->getOnlinePlayers() as $ps){
+			if(!$ps->isSneaking() && $ps->distance($p) < 10){
+				$p->attack(100000, new EntityDamageEvent($p, EntityDamageEvent::CAUSE_MAGIC, 100000));
+				break;
+			}
+		}
+	}
+
+
+}
